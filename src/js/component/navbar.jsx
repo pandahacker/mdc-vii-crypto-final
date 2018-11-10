951dc2d9a1db9ddfac41734615c49a0bbd073273
Blake Albertson <albertson.blake@yahoo.com>
2018-11-10 15:33:30 -0500

    navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
 
 export class Navbar extends React.Component {
 	render() {
-		return <h2 className="text-center">Navbar</h2>;
+		const menu = [
+			{ label: "REGISTER", url: "register" },
+			{ label: "PRICING", url: "pricing" },
+			{ label: "ABOUT US", url: "about" },
+			{ label: "CONTACT", url: "contact" }
+		];
+
+		const items = menu.map(item => (
+			<li key={item.index} className="nav-item">
+				<Link className="nav-link" to={`/${item.url}`}>
+					{item.label}
+				</Link>
+			</li>
+		));
+
+		return (
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<i className="navbar-brand fas fa-coins" />
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav mr-auto">{items}</ul>
+				</div>
+			</nav>
+		);
 	}
 }
