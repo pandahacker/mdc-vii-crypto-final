ef70218ac254e5f7e698ba690627e2ae7b702925
Henry Orama <henryorama07@gmail.com>
2018-11-15 22:49:07 +0000

    Pricing page rough draft

--- src/js/views/pricing.jsx
+++ src/js/views/pricing.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import {
+	Container,
+	Row,
+	Button,
+	Card,
+	CardBody,
+	CardImage,
+	CardTitle,
+	CardText,
+	Col
+} from "mdbreact";
 
 export class Pricing extends React.Component {
 	render() {
-		return <div />;
+		return (
+			<Container>
+				<div align="right">
+					<div clasNames="card">
+						<h5 className="card-header">
+							<center>30 day trial</center>
+						</h5>
+						<div className="card-body">
+							<div align="left">
+								<CardImage src="https://via.placeholder.com/150" />
+							</div>
+							<h5 className="card-title">
+								<div align="right">20 USD per month</div>
+							</h5>
+							<p className="card-text">
+								<ul>
+									<p>Access to membership features</p>
+									<p>Greeting: &quot;Hello, World!&quot;</p>
+									<p>Greeting: &quot;Hello, World!&quot;</p>
+								</ul>
+							</p>
+							<a href="#" className="btn btn-primary">
+								Order
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div align="right">
+					<div clasNames="card">
+						<h5 className="card-header">
+							<center>Personal</center>
+						</h5>
+						<div className="card-body">
+							<div align="left">
+								{" "}
+								<CardImage src="https://via.placeholder.com/150" />{" "}
+							</div>
+							<h5 className="card-title">40 USD per month</h5>
+							<p className="card-text">
+								<ul>
+									<p>Access to membership features</p>
+									<p>Greeting: &quot;Hello, World!&quot;</p>
+									<p>Greeting: &quot;Hello, World!&quot;</p>
+								</ul>
+							</p>
+							<a href="#" className="btn btn-primary">
+								Order
+							</a>
+						</div>
+					</div>
+				</div>
+			</Container>
+		);
 	}
 }
